58394f2662a21277dec7945deda1a64d2c078092
Ayan Roy <154493815+Ayanroy004@users.noreply.github.com>
2026-08-15 00:42:55 +0530

    debug api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 console.log("Production API URL:", import.meta.env.VITE_API_URL);
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: "https://medio-physiotheraphy.onrender.com/api",
   withCredentials: true,
 });
 
